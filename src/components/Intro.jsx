e372b7f208e181741cf3b5d3604bf862a41d6dc3
Umar Farooq <149324609+umarfarooq6519@users.noreply.github.com>
2024-09-02 16:49:00 +0500

    update title name and icon

---
+++ src/components/Intro.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/no-unescaped-entities */
+
+import Button from "./elements/Button";
+
+export default function Intro() {
+  return (
+    <section id="intro" className="">
+    
+    </section>
+  );
+}
